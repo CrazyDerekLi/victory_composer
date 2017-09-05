18290ffe4927a0975d2ce27d16823e121b11a866
Derek Li <327228789@qq.com>
2017-09-05 10:54:53 +0800

    jenkins
    jenkins

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -5,7 +5,16 @@
 
 echo "npm begin"
 cd ./victory_zonghe/yitihua/
-echo "LoggingCurveCom"
+echo "WebJsComposer"
+npm link gulp
+npm link gulp-zip
+npm run test
+npm publish
+
+
+echo "npm begin"
+cd ../../victory_shuju/drag_layout/
+echo "DragLayout"
 npm link gulp
 npm link gulp-zip
 npm run test
